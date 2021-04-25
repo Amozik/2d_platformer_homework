c78b547d5cfac752b61e2a5d77091355ed2999f8
Amozik <amozik@rambler.ru>
2021-04-25 15:16:19 +0200

    add some physics

---
+++ Assets/Scripts/Controllers/LevelInitialization.cs
@@ -0,0 +1,16 @@
+﻿using Platformer.Levels;
+using UnityEngine;
+
+namespace Platformer.Controllers
+{
+    public class LevelInitialization
+    {
+        public BaseLevel Level { get; }
+
+        public LevelInitialization()
+        {
+            //TODO: Инициализировать из префаба, который будет указан в конфиге
+            Level = Object.FindObjectOfType<BaseLevel>();
+        }
+    }
+}
